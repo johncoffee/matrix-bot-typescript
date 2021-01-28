6d4be6ccb5cd68c47a6ee8e565d596748f81fd73
johncoffee <github2020@studio42.dk>
2021-01-28 16:58:51 +0100

    screenshot tool

--- src/cypress/integration/getWeather.spec.js
+++ src/cypress/integration/getWeather.spec.js
@@ -1,8 +1,15 @@
 describe('get weather', () => {
 
-  it('da stuff', () =>  {
-    cy.visit('/nb/værvarsel/daglig-tabell/2-2618425/Danmark/Region Hovedstaden/København/København', {failOnStatusCode: true})
-    // cy.scrollIntoView("")
+  it('should take a screenshot', () => {
+    cy.pause()
+    cy.visit('/nb/værvarsel/daglig-tabell/2-2618425/Danmark/Region Hovedstaden/København/København', {
+      failOnStatusCode: true
+    })
+    cy.get('.daily-weather-list__headers')
+      .first().scrollIntoView()
+    cy.screenshot({
+      capture: "viewport"
+    })
   })
 
 })
